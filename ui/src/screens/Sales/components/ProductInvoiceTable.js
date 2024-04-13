29a943490624ed79a368c8b71e20edf7e6509511
Umansh <usarabhai@gmail.com>
2024-04-13 22:15:50 +0530

    feat: Invoice paid module introduced

--- ui/src/screens/Sales/components/ProductInvoiceTable.js
+++ ui/src/screens/Sales/components/ProductInvoiceTable.js
@@ -1,5 +1,4 @@
 import { Card, Typography, Button } from "@material-tailwind/react";
-import { Icon } from '@material-tailwind/react';
 
 export function ProductInvoiceTable({
   TABLE_HEAD,
@@ -47,12 +46,28 @@ export function ProductInvoiceTable({
                   <td className={classes} key={idx}>
                     {values[v] === "DELETE" ? (
                       <Button
-                        color="red"
+                        color="white"
                         size="xs" // Adjusted button size to xs
                         onClick={() => handleDeleteRow(index)}
                         className="py-1 px-2" // Adjusted padding
                       >
-                        Delete
+                        <svg
+                          class="w-6 h-6 text-gray-800 dark:text-white"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+                          />
+                        </svg>
                       </Button>
                     ) : (
                       <Typography
