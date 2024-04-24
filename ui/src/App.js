f337bf1e0441f7ba1a4f8d7f269ea272fed8ecab
Umansh <usarabhai@gmail.com>
2024-04-24 11:36:36 +0530

    style: layout style changes

--- ui/src/App.js
+++ ui/src/App.js
@@ -49,21 +49,24 @@ function App() {
                 <button
                   className="cursor-pointer duration-200 hover:scale-110 active:scale-100"
                   title="Go Back"
-                  style={{ border: "1px solid", marginLeft: 20 }}
+                  style={{ border: "1px solid", marginLeft: 20, padding: 8 }}
                 >
                   <svg
+                    class="w-6 h-6 text-gray-800 dark:text-white"
+                    aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
-                    width="50px"
-                    height="30px"
+                    width="24"
+                    height="24"
+                    fill="none"
                     viewBox="0 0 24 24"
-                    className="stroke-black"
                   >
                     <path
-                      strokeLinejoin="round"
-                      strokeLinecap="round"
-                      strokeWidth="1.5"
-                      d="M11 6L5 12M5 12L11 18M5 12H19"
-                    ></path>
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"
+                    />
                   </svg>
                 </button>
               </Link>
@@ -92,21 +95,29 @@ function App() {
               <button
                 className="cursor-pointer duration-200 hover:scale-110 active:scale-100"
                 title="Go Back"
-                style={{ border: "1px solid", marginLeft: 20 }}
+                style={{
+                  border: "1px solid",
+                  marginLeft: 10,
+                  padding: 8,
+                  marginBottom: 2,
+                }}
               >
                 <svg
+                  class="w-6 h-6 text-gray-800 dark:text-white"
+                  aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
-                  width="50px"
-                  height="30px"
+                  width="24"
+                  height="24"
+                  fill="none"
                   viewBox="0 0 24 24"
-                  className="stroke-black"
                 >
                   <path
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="1.5"
-                    d="M11 6L5 12M5 12L11 18M5 12H19"
-                  ></path>
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"
+                  />
                 </svg>
               </button>
             </Link>
